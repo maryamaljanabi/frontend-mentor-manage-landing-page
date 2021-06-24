2b45918411145964b10924a2bfd38a9c3c292a31
maryamaljanabi <maryam.aljanabi135@gmail.com>
2021-06-24 13:58:46 +0300

    Bottom orange banner

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top-banner">
+        <h3>Simplify how your team works today.</h3>
+        <button className="rounded-orange-button inverted-rounded-orange-button">
+          Get Started
+        </button>
+      </div>
+      <div className="bottom-footer"></div>
+    </div>
+  );
 };
 
 export default Footer;
